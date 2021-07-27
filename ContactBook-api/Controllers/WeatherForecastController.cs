a85e44eb1696e7babd5cf4a970b328e5a8077066
Aemiro Mekete <aemiromekete12@gmail.com>
2021-07-27 17:43:03 +0300

    Structure for my Project

--- ContactBook-api/Controllers/WeatherForecastController.cs
+++ ContactBook-api/Controllers/WeatherForecastController.cs
@@ -3,9 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-
-namespace ContactBook-api.Controllers
+namespace ContactBook.api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
